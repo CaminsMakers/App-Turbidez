23afffdb081044a8583f6ed4645e3c8414c7069f
Camins Makers <caminsmakers@gmail.com>
2017-05-02 16:28:16 +0200

    Add files via upload

---
+++ intensidad_luz.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+
+#define rxPin 1
+#define txPin 2
+
+int luz_1 = A5;
+int lectura_luz;
+int turbiedad;
+
+
+SoftwareSerial BTSerial (rxPin, txPin);
+
+void setup() {
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  
+lectura_luz = analogRead(luz_1);
+turbiedad=map(lectura_luz,0,155,100,0);
+
+if(turbiedad<0){
+  turbiedad=0;
+}
+if(turbiedad>100){
+  turbiedad=100;
+}
+
+//Serial.println(lectura_luz);
+Serial.println(turbiedad);
+delay(1000);
+}
+
